0be43539e02c07fab7235437a1a1035b74c8f7e5
German Monfort <german.monfort@gmail.com>
2012-10-03 01:02:05 -0300

    Parsing response data

--- run.rb
+++ run.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 
 require 'net/http'
-# require 'hpricot'
+require 'hpricot'
 
-# uri = URI("http://www.trescruces.com.uy/horarios.php?origen=1&destino=23&compania_o=0&buscar=buscar")
-#
-# res = Net::HTTP.get(uri)
+class Record
+  attr_accessor :company, :phone, :depart_time, :frequency, :road, :travel_distance, :travel_duration
+end
 
 uri = URI("http://www.trescruces.com.uy/horarios.php")
 params = {
@@ -17,4 +17,28 @@ params = {
 uri.query = URI.encode_www_form(params)
 
 res = Net::HTTP.get_response(uri)
-puts res.body
+
+if res.is_a?(Net::HTTPSuccess)
+  doc = Hpricot(res.body)
+  table = doc.at("#horariosdiv table")
+  data = table.search('tr')
+
+  records = []
+
+  data.each do |tr|
+    record = Record.new
+    record.company          = tr.search('td')[0].inner_html
+    record.phone            = tr.search('td')[1].inner_html
+    record.depart_time      = tr.search('td')[2].inner_html
+    record.frequency        = tr.search('td')[3].inner_html
+    record.road             = tr.search('td')[4].inner_html
+    record.travel_distance  = tr.search('td')[5].inner_html
+    record.travel_duration  = tr.search('td')[6].inner_html
+
+    records << record
+  end
+
+  records.each {|r| puts r.inspect }
+else
+  puts "Error: %s" % res.inspect
+end
